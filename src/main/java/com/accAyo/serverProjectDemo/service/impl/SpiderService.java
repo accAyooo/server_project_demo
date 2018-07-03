0145d8d05ab300dfd187e4287336b870e233659a
shixiangyu216 <773681606@qq.com>
2018-07-03 18:34:28 +0800

    爬虫管理

---
+++ src/main/java/com/accAyo/serverProjectDemo/service/impl/SpiderService.java
@@ -0,0 +1,16 @@
+package com.accAyo.serverProjectDemo.service.impl;
+
+import com.accAyo.serverProjectDemo.service.ISpiderService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Desc:
+ *
+ * @author shixiangyu
+ * @date 2018/7/3
+ */
+
+@Service
+public class SpiderService implements ISpiderService {
+
+}
